11b4f867a890fb9f86e515aa13278111c97b9f2f
Brehcore <brehcore@gmail.com>
2026-05-04 12:08:23 -0300

    feat: adiciona logo teste na página de login  e melhora a página de perfil.

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,46 +1,60 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from '../../core/services/auth.service';
-import { FamilyDTO } from '../../core/services/models/auth.models';
+import { Navbar } from '../../shared/navbar/navbar';
+
+
+export interface UserResponseDTO {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  familyId: number;
+  familyName: string;
+  profilePictureUrl: string;
+}
 
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Navbar],
   templateUrl: './profile.html',
   styleUrls: ['./profile.scss']
 })
-export class Profile {
+export class Profile implements OnInit {
   private router = inject(Router);
   private authService = inject(AuthService);
+  private http = inject(HttpClient);
+  private readonly API_BASE = 'http://localhost:8082';
 
-  families: FamilyDTO[] = [];
+
+  user: UserResponseDTO | null = null;
   loading = false;
+  error = '';
 
   ngOnInit(): void {
     this.loading = true;
-    this.authService.getMyFamilies().subscribe({
+
+    const token = this.authService.getToken();
+    const httpHeaders = token
+      ? new HttpHeaders({ 'Authorization': `Bearer ${token}` })
+      : undefined;
+
+      const options = httpHeaders ? { headers: httpHeaders } : {};
+
+    this.http.get<UserResponseDTO>(`${this.API_BASE}/api/users/me`, options).subscribe({
       next: (res) => {
-        this.families = res || [];
+        this.user = res;
         this.loading = false;
       },
       error: (err) => {
-        console.error('Erro ao buscar famílias:', err);
+        console.error('Erro ao buscar perfil:', err);
+        this.error = 'Não foi possível carregar os dados do perfil.';
         this.loading = false;
       }
     });
-  }
-
-  selectFamily(id: string) {
-    try {
-      // Armazenar em sessionStorage para reduzir persistência além da sessão
-      sessionStorage.setItem('selectedFamilyId', id);
-    } catch (e) {
-      console.warn('Não foi possível salvar sessionStorage, fallback para localStorage', e);
-      localStorage.setItem('selectedFamilyId', id);
-    }
-    this.router.navigate(['/home'], { queryParams: { family: id } });
   }
 }
